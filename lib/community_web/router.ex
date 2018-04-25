f71522b6034fbbfba63e8a4845442b5875f280b2
junior <juniornelson123>
2018-04-25 15:36:50 -0300

    base

---
+++ lib/community_web/router.ex
@@ -0,0 +1,15 @@
+defmodule CommunityWeb.Router do
+  use CommunityWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+
+
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug,schema: CommunityWeb.Schema
+  end
+end
